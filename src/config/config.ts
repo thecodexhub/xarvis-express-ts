f1e0d685b9ef2d7973b6840af5fafe1807ba2cc5
Sandip <sandipramanik07@gmail.com>
2023-09-13 23:55:26 +0530

    Add env validations with Joi, install cross-env and pm2

---
+++ src/config/config.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import Joi from 'joi';
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+interface EnvVars {
+  NODE_ENV: 'production' | 'development' | 'test';
+  PORT: number;
+}
+
+const envVarsSchema = Joi.object<EnvVars>({
+  NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+  PORT: Joi.number().default(3000),
+}).unknown();
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+export const env = envVars.NODE_ENV;
+export const port = envVars.PORT;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { Express } from 'express';
+import * as config from './config/config';
 
 const app: Express = express();
-const port: number = Number(process.env.PORT) || 3000;
+const port: number = config.port;
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running on PORT ${port}`);
